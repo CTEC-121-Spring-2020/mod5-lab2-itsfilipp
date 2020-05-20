707869629dfe6b620d5c6760156d0f5312ea5651
Filipp Kopytyuk <f.kopytyuk@students.clark.edu>
2020-05-20 12:16:59 -0700

    Followed lab but did not complete.

--- Template.py
+++ Template.py
@@ -1,6 +1,6 @@
 """
 CTEC 121
-<your name>
+Filipp Kopytyuk
 <assignment/lab name>
 <assignment/lab description
 """
@@ -10,8 +10,48 @@ Input(s): list/description
 Process: description of what function does
 Output: return value and description
 """
+'''
+def verse(name):
+    print()
+    print(name, " finger, ",name, " finger, ")
+    print("Here I am, here I am. How do you do?\n")
+
+def singFamilySong():
+
+    for name in ["Daddy", "Mommy", "Brother", "Sister", "Baby"]
+        verse(name)
 
 def main():
-    # code here
+    singFamilySong()
 
 main()    
+
+def main():
+    Song("Daddy")
+    Song("Mommy")
+    Song("Brother")
+    Song("Sister")
+    Song("Baby")
+def Song(name):
+    print(name, "finger,", name, "finger, where are you?\nHere I am, here I am. How do you do?\n")
+main() 
+'''
+
+def AgeClass(age):
+    if age < 2:
+        return 'I'
+    elif age <13:
+        return 'C'
+    elif age < 18:
+        return 'T'
+    elif age <= 130:
+        return 'A'
+    else:
+        return 'U'
+    
+    def testAgeClass():
+        print()
+        print(AgeClass(-3), "expect U: -3 input")
+        print(AgeClass(0), "expect I: 0 input")
+        print(AgeClass(1), "expect I: 1 input")
+        print(AgeClass(2), )
